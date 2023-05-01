dd80c54d1bec96f430e7df4aa8ce8e0b817db98a
Ango <ab9tm@virginia.edu>
2023-04-30 21:08:48 -0400

    update home page / games played aesthetic

--- site/games_played.php
+++ site/games_played.php
@@ -1,33 +1,33 @@
 <?php
-require("..\util\\connect-db.php");
-require("..\util\\user-db.php");
-require("..\util\\game-db.php");
-require("..\util\\addPlay-db.php");
+    require("..\util\\connect-db.php");
+    require("..\util\\user-db.php");
+    require("..\util\\game-db.php");
+    require("..\util\\addPlay-db.php");
 
-session_start();
+    session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    // User is not logged in, redirect them to the login page
-    header("Location: login.php");
-    exit;
-}
-
-$user_id = $_SESSION['user_id'];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['add_game_played'])) {
-        $game_id = $_POST['add_game_played'];
-        $hours_played = $_POST['add_hours_played'];
-        addGamePlayed($user_id, $game_id, $hours_played);
-    } elseif (isset($_POST['remove_game_played'])) {
-        $game_id = $_POST['remove_game_played'];
-        deleteGamePlayed($user_id,$game_id);
+    if (!isset($_SESSION['user_id'])) {
+        // User is not logged in, redirect them to the login page
+        header("Location: login.php");
+        exit;
     }
-}
 
-$games = getGamesPlayed($user_id);
+    $user_id = $_SESSION['user_id'];
 
-$allGames = getAllGames();
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['add_game_played'])) {
+            $game_id = $_POST['add_game_played'];
+            $hours_played = $_POST['add_hours_played'];
+            addGamePlayed($user_id, $game_id, $hours_played);
+        } elseif (isset($_POST['remove_game_played'])) {
+            $game_id = $_POST['remove_game_played'];
+            deleteGamePlayed($user_id,$game_id);
+        }
+    }
+
+    $games = getGamesPlayed($user_id);
+
+    $allGames = getAllGames();
 ?>
 
 <!DOCTYPE html>
@@ -37,89 +37,106 @@ $allGames = getAllGames();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Games Played</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    <img src="../../img/logo.png" alt="Logo" style="max-height: 100%; max-width: 100%;">
-                </a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="../site">Home</a></li>
-                <li class="active"><a href="games_played.php">Games Played</a></li>
-                <li><a href="game_info.php">Game Search</a></li>
-                <li><a href="user_info.php">Users</a></li>
-                <li><a href="friends.php">Friends</a></li>
-                <li><a href="write_review.php">Review</a></li>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"><img src="../../img/logo.png" alt="Logo" style="max-height: 25%; max-width: 25%;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../site">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="games_played.php">Games Played</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="game_info.php">Game Search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="user_info.php">Users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="friends.php">Friends</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="write_review.php">Review</a>
+                </li>
             </ul>
-            <form class="navbar-form navbar-left">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search for games, users, ...">
-                </div>
-                <button type="submit" class="btn btn-default">Search</button>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search for games, users, ..." aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
             </form>
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="navbar-nav ml-auto">
+                <?php if (!isset($_SESSION['user_id'])): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php"><span class="glyphicon glyphicon-log-in"></span> Log in</a>
+                </li>
+            <?php else: ?>
                 <?php
-                //session_start();
-                if (!isset($_SESSION['user_id'])) {
-                    echo '<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>';
-                } else {
-                    $user_id = $_SESSION['user_id'];
-                    $full_name = getFullName($user_id);
-                    echo '<li><a href="user_info.php"><span class="glyphicon glyphicon-user"></span> ' . $full_name[0][0] . ' ' . $full_name[0][1] . '</a></li>';
-                    echo '<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>';
-                }
+                $user_id = $_SESSION['user_id'];
+                $full_name = getFullName($user_id);
                 ?>
-            </ul>
-        </div>
-    </nav>
+                <li class="nav-item">
+                    <a class="nav-link" href="user_info.php"><span class="glyphicon glyphicon-user"></span> <?php echo $full_name[0][0] . ' ' . $full_name[0][1]; ?></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Log out</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </div>
+</nav>
 
-    <div class="container">
-        <h1 class="mt-5">Games Played</h1>
-        <div class="row">
-            <div class="col-md-6">
+<div class="container mt-5">
+    <h1>Games Played</h1>
+    <div class="row">
+        <div class="col-md-6">
             <h2>My Games</h2>
-                <?php if (empty($games)) { ?>
-                    <p>You have no games added yet.</p>
-                <?php } else { ?>
-                        <ul class="list-group">
-                            <?php foreach ($games as $game) { ?>
-                                <li class="list-group-item"> Title: <?php echo $game["title"]; ?>,
-                                 Game ID: <?php echo $game["game_id"]; ?></li>
-                            <?php } ?>
-                        </ul>
-                    <?php } ?>
-            </div>
+            <?php if (empty($games)): ?>
+                <p>You have no games added yet.</p>
+            <?php else: ?>
+                <ul class="list-group">
+                    <?php foreach ($games as $game): ?>
+                        <li class="list-group-item">Title: <?php echo $game["title"]; ?>, Game ID: <?php echo $game["game_id"]; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
         </div>
-        <hr>
-        <div class="row">
-            <div class="col-md-6">
-                <h2>Add a Game</h2>
-                <form action="games_played.php" method="post">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Enter a game ID" name="add_game_played" required>
-                        <input type="text" class="form-control" placeholder="Enter number of hours played as an int!" name="add_hours_played" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Add a Game you have Played!</button>
-                </form>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-md-6">
-                <h2>Remove a Game from Played</h2>
-                
-                <form action="games_played.php" method="post">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Enter a Game ID" name="remove_game_played" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Remove Game Played</button>
-                </form>
-            </div>
+        <div class="col-md-6">
+            <h2>Add a Game</h2>
+            <form action="games_played.php" method="post">
+                <div class="form-group">
+                    <label for="add_game_played">Game ID</label>
+                    <input type="text" class="form-control" id="add_game_played" name="add_game_played" placeholder="Enter a game ID" required>
+                </div>
+                <div class="form-group">
+                    <label for="add_hours_played">Hours Played</label>
+                    <input type="number" class="form-control" id="add_hours_played" name="add_hours_played" placeholder="Enter number of hours played as an int!" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Add a Game you have Played!</button>
+            </form>
         </div>
     </div>
-    </body>
-</html>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Remove a Game from Played</h2>
+            <form action="games_played.php" method="post">
+                <div class="form-group">
+                    <label for="remove_game_played">Game ID</label>
+                    <input type="text" class="form-control" id="remove_game_played" name="remove_game_played" placeholder="Enter a Game ID" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Remove Game Played</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
--- site/home.php
+++ site/home.php
@@ -15,7 +15,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['search'])) {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,52 +22,151 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['search'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f5f5f5;
+        }
+
+        .navbar {
+            background-color: #343a40;
+        }
+
+        .navbar-brand,
+        .navbar-nav .nav-link {
+            color: #fff;
+        }
+
+        .navbar-brand img {
+            max-height: 25%;
+            max-width: 25%;
+        }
+
+        .jumbotron {
+            background-image: url("../../img/bg.jpg");
+            background-size: cover;
+            background-position: center;
+            height: 300px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .jumbotron h1 {
+            color: #fff;
+            text-shadow: 1px 1px #000;
+            font-weight: bold;
+            font-size: 5rem;
+            text-align: center;
+        }
+
+        .jumbotron p {
+            color: #fff;
+            text-shadow: 1px 1px #000;
+            font-size: 2rem;
+            text-align: center;
+            margin-top: 1rem;
+        }
+
+        .search-form {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-top: 2rem;
+        }
+
+        .search-input {
+            border-radius: 0;
+        }
+    </style>
 </head>
 <body>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    <img src="../../img/logo.png" alt="Logo" style="max-height: 100%; max-width: 100%;">
-                </a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="../site">Home</a></li>
-                <li><a href="games_played.php">Games Played</a></li>
-                <li><a href="game_info.php">Game Search</a></li>
-                <li><a href="user_info.php">Users</a></li>
-                <li><a href="friends.php">Friends</a></li>
-                <li><a href="write_review.php">Review</a></li>
-            </ul>
-            <form class="navbar-form navbar-left">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search for games, users, ...">
+            <a class="navbar-brand" href="#">
+                <img src="../../img/logo.png" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../site">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="games_played.php">Games Played</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="game_info.php">Game Search</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="user_info.php">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="friends.php">Friends</a>
+                    </li>
+                    <li  class="nav-item">
+                            <a class="nav-link" href="write_review.php">Review</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline ml-auto" method="post">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for games, users, ..." name="search_query">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-light" type="submit" name="search"><i class="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                    <?php
+                    if (!isset($_SESSION['user_id'])) {
+                        echo '<a class="btn btn-outline-light ml-3" href="login.php">Log in</a>';
+                    } else {
+                        $user_id = $_SESSION['user_id'];
+                        $full_name = getFullName($user_id);
+                        echo '<a class="btn btn-outline-light ml-3" href="user_info.php"><i class="fa fa-user"></i> ' . $full_name[0][0] . ' ' . $full_name[0][1] . '</a>';
+                        echo '<a class="btn btn-outline-light ml-3" href="logout.php">Log out</a>';
+                    }
+                    ?>
                 </div>
-                <button type="submit" class="btn btn-default">Search</button>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <?php
-                //session_start();
-                if (!isset($_SESSION['user_id'])) {
-                    echo '<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>';
-                } else {
-                    $user_id = $_SESSION['user_id'];
-                    $full_name = getFullName($user_id);
-                    echo '<li><a href="user_info.php"><span class="glyphicon glyphicon-user"></span> ' . $full_name[0][0] . ' ' . $full_name[0][1] . '</a></li>';
-                    echo '<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>';
-                }
-                ?>
-            </ul>
+            </nav>
         </div>
-    </nav>
+    </div>
+</nav>
 
+<div class="jumbotron">
     <div class="container">
         <h1>Welcome to PlayTracker!</h1>
         <p>Here you can find information about various games and connect with other gamers from around the world.</p>
+        <div class="search-form">
+            <form method="post">
+                <div class="input-group">
+                    <input type="text" class="form-control search-input" placeholder="Search for games, users, ..." name="search_query">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit" name="search"><i class="fa fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-</html>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Games</h2>
+            <p>Browse our collection of games and find your new favorite.</p>
+            <a class="btn btn-primary" href="game_info.php">Browse Games</a>
+        </div>
+        <div class="col-md-4">
+            <h2>Users</h2>
+            <p>Connect with other gamers from around the world and build your network.</p>
+            <a class="btn btn-primary" href="user_info.php">Browse Users</a>
+        </div>
+        <div class="col-md-4">
+            <h2>Reviews</h2>
+            <p>Read reviews of games written by other players or share your own thoughts and experiences.</p>
+            <a class="btn btn-primary" href="write_review.php">Write a Review</a>
+        </div>
+    </div>
+</div>
